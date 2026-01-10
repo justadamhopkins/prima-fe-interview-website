45a3ef893951afb94a98215860057c0c84c816fe
Adam Hopkins <adamhopkins87@gmail.com>
2026-01-10 16:54:58 +0000

    refactor: index css entry point

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "@styles/tokens/base/reset.css";
 import "@styles/tokens/base/index.css";
 
 import { App } from "@components/_templates/App";
