4ec81496255fddda7f859d404c8c35a9322d9f9c
adamhopkins <adamhopkins87@gmail.com>
2026-01-10 00:16:44 +0000

    chore: base setup files

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@styles/reset.css";
-import "@styles/index.css";
+
+import "@styles/tokens/base/reset.css";
+import "@styles/tokens/base/index.css";
+
+import { App } from "@components/_templates/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
-root.render(<React.StrictMode>👋</React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
