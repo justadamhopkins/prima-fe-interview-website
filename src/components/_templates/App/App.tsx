af3354ea734995e3d66adc77cba0e2aee89abd1c
adamhopkins <adamhopkins87@gmail.com>
2026-01-14 00:33:33 +0000

    feat: added a route declaration

--- src/components/_templates/App/App.tsx
+++ src/components/_templates/App/App.tsx
@@ -1,10 +1,13 @@
 import { SiteContainer } from "@atoms/SiteContainer";
 import { HomePage } from "@components/_templates/HomePage";
+import { Route, Routes } from "react-router";
 
 export const App = () => {
   return (
     <SiteContainer>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </SiteContainer>
   );
 };
