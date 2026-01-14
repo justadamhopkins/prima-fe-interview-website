b5505ac9890282a92f1d06d5e56abdb23240cfe6
adamhopkins <adamhopkins87@gmail.com>
2026-01-14 00:29:12 +0000

    feat: added home page css

--- src/components/_templates/HomePage/HomePage.tsx
+++ src/components/_templates/HomePage/HomePage.tsx
@@ -1,13 +1,25 @@
+import { HIGHLIGHT_VARIANTS, Highlight } from "@atoms/Highlight";
+import { ROLES } from "@constants/roles.ts";
+import { FilterBar } from "@molecules/FilterBar";
+import { SearchInput } from "@molecules/SearchInput";
 import styles from "./Homepage.module.css";
 
 export const HomePage = () => {
   return (
     <div className={styles.homepage}>
-      <header>
+      <header className={styles.homepage__header}>
         <h1 className={styles.homepage__heading}>
-          <span>User</span> Dashboard
+          <Highlight variant={HIGHLIGHT_VARIANTS.PRIMARY}>User</Highlight> Dashboard
         </h1>
+        <SearchInput onChange={() => null} handleSearch={() => null} value="" />
       </header>
+      <div className={styles.homepage__container}>
+        <FilterBar
+          className={styles.homepage__filterWrapper}
+          roles={Object.values(ROLES)}
+          handleFilterSelection={() => null}
+        />
+      </div>
     </div>
   );
 };
