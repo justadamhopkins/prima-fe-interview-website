524ffe70ad2f8858372e3c8c9de13194f5ca9b7f
adamhopkins <adamhopkins87@gmail.com>
2026-01-14 00:31:26 +0000

    feat: added react router

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import ReactDOM from "react-dom/client";
 import "@styles/tokens/base/index.css";
 
 import { App } from "@components/_templates/App";
+import { BrowserRouter } from "react-router";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
